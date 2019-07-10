7bf2211d5c21b8c6761349eff3b3146593a0aba6
zhenwu93 <zhenwu93@gmail.com>
2019-07-10 18:12:17 -0400

    created Contact and Home components. Added routes to App.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react'
+import { Route, NavLink, HashRouter } from "react-router-dom";
+import Home from "./Home";
+import Contact from "./Contact";
+
+//TODO
+// import Signup from "./SignUp";
+// import LogIn from "./LogIn";
+//<Route path="/signup" component={SignUp}/>
+//<Route path="/login" component={LogIn}/>
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Howl for Change</h1>
-        <ul className="header">
-          <li><a href="/">Home</a></li>
-          <li><a href="/signup">Sign Up</a></li>
-          <li><a href="/login">Log In</a></li>
-          <li><a href="/contact">Contact</a></li>
-        </ul>
+      <HashRouter>
+        <div>
+          <h1>Howl for Change</h1>
+          <ul className="header">
+            <li><a href="/">Home</a></li>
+            <li><a href="/signup">Sign Up</a></li>
+            <li><a href="/login">Log In</a></li>
+            <li><a href="/contact">Contact</a></li>
+            </ul>
 
-        <div className="content">
-
-        </div>
-      </div>
+            <div className="content">
+            <Route path="/" component={Home}/>
+            
+            <Route path="/contact" component={Contact}/>
+            </div>
+          </div>
+      </HashRouter>
     );
   }
 }
