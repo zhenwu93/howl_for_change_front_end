0bd2dad6544ae308bf4e4fc611f34aa9df83dc94
zhenwu93 <zhenwu93@gmail.com>
2019-07-12 15:00:10 -0400

    added phone number field to subscribe

--- src/Components/Subscribe.js
+++ src/Components/Subscribe.js
@@ -7,6 +7,7 @@ class Subscribe extends React.Component {
         emailValue: '',
         fNameValue: '',
         lNameValue: '',
+        phoneNumber: '',
     };
   }
 
@@ -15,38 +16,53 @@ class Subscribe extends React.Component {
                 <form action="https://howlforchange.us19.list-manage.com/subscribe/post" method="POST" noValidate>
                   <input type="hidden" name="u" value="6397bd73322c1f70600e82d2e"/>
                   <input type="hidden" name="id" value="ded48d18b3"/>
+
                 <label htmlFor='MERGE0'>
-                    Email
-                    <input
-                        type="email"
-                        name="EMAIL"
-                        id="MERGE0"
-                        value={this.state.emailValue}
-                        onChange={ (e)=>{this.setState({emailValue: e.target.value});} }
-                        autoCapitalize="off"
-                        autoCorrect="off"
-                     />
-                </label>
-                <label htmlFor='MERGE1'>
                     First name
                     <input
                         type="text"
                         name="FNAME"
-                        id="MERGE1"
+                        id="MERGE0"
                         value={this.state.fNameValue}
                         onChange={(e)=>{this.setState({fNameValue: e.target.value});}}
                     />
                 </label>
-                <label htmlFor='MERGE2'>
+
+                <label htmlFor='MERGE1'>
                     Last name
                     <input
                         type="text"
                         name="LNAME"
-                        id="MERGE2"
+                        id="MERGE1"
                         value={this.state.lNameValue}
                         onChange={(e)=>{this.setState({lNameValue: e.target.value});}}
                     />
                 </label>
+
+                <label htmlFor='MERGE2'>
+                    Email
+                    <input
+                        type="email"
+                        name="EMAIL"
+                        id="MERGE2"
+                        value={this.state.emailValue}
+                        onChange={ (e)=>{this.setState({emailValue: e.target.value});} }
+                        autoCapitalize="off"
+                        autoCorrect="off"
+                    />
+                </label>
+
+                <label htmlFor='MERGE3'>
+                    Phone Number
+                    <input
+                        type="phone"
+                        name="PHONE"
+                        id="MERGE3"
+                        value={this.state.phoneNumber}
+                        onChange={ (e)=>{this.setState({phoneNumber: e.target.value});} }
+                    />
+                </label>
+
                   <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button"/>
 
                 <div style={{position: 'absolute', left: '-5000px'}} aria-hidden='true' aria-label="Please leave the following three fields empty">
