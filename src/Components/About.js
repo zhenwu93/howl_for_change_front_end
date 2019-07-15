709b1932c19fe292772be8646cd5d1fa0448dc75
jtang93 <tang.jonathan93@gmail.com>
2019-07-15 05:36:23 -0400

    styling and formatting changes, vert centered topnav links

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,36 +1,19 @@
 import React, { Component } from "react";
-import Subscribe from "./Subscribe";
 
 class About extends Component {
   render() {
     return (
       <div class="wrapperDiv">
         <section>
-          <div class="container">
+          <div class="landing container">
             <h2>Landing Section</h2>
-            <Subscribe>
-            </Subscribe>
-            <p>Cras facilisis urna ornare ex volutpat, et
-            convallis erat elementum. Ut aliquam, ipsum vitae
-            gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-            metus nec massa. Maecenas hendrerit laoreet augue
-            nec molestie. Cum sociis natoque penatibus et magnis
-            dis parturient montes, nascetur ridiculus mus.</p>
-
-            <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+            <p>Insert 'Who we are and what we are about text'</p>
           </div>
         </section>
         <section>
           <div class="container">
             <h2>Gallery</h2>
-            <p>Cras facilisis urna ornare ex volutpat, et
-            convallis erat elementum. Ut aliquam, ipsum vitae
-            gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-            metus nec massa. Maecenas hendrerit laoreet augue
-            nec molestie. Cum sociis natoque penatibus et magnis
-            dis parturient montes, nascetur ridiculus mus.</p>
-
-            <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+            <p>Insert Pictures for Gallery</p>
           </div>
         </section>
         <section>
@@ -49,14 +32,13 @@ class About extends Component {
         <section>
           <div class="container">
             <h2>How HFC Works</h2>
-            <p>Cras facilisis urna ornare ex volutpat, et
-            convallis erat elementum. Ut aliquam, ipsum vitae
-            gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-            metus nec massa. Maecenas hendrerit laoreet augue
-            nec molestie. Cum sociis natoque penatibus et magnis
-            dis parturient montes, nascetur ridiculus mus.</p>
-
-            <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+            <p>Step by Step process</p>
+          </div>
+        </section>
+        <section>
+          <div class="container">
+            <h2>Footer</h2>
+            <p>Footer Text/links</p>
           </div>
         </section>
       </div>
