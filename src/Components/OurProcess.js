0af50b6e9337dd279c1719b5ea5a25be3605bf98
jtang93 <tang.jonathan93@gmail.com>
2019-07-18 15:50:35 -0400

    styling changes to process and invite buttons

--- src/Components/OurProcess.js
+++ src/Components/OurProcess.js
@@ -1,4 +1,6 @@
 import React, {Component} from "react"
+import {NavLink} from "react-router-dom";
+import ReqInviteButton from './ReqInviteButton'
 import process1 from "../assets/process/process1.png"
 import process2 from "../assets/process/process2.png"
 import process3 from "../assets/process/process3.png"
@@ -8,28 +10,43 @@ class OurProcess extends Component {
   render() {
     return (
       <section>
-        <div className="ourprocess container">
-          <h1>Our Process</h1>
+        <div className="ourprocess-ctn container">
+          <p className='headertext'>Our Process</p>
 
-          <div className="process1 container">
-            <h2>1.</h2><p>NONPROFITS CREATE A CALL FOR SUBMISSIONS FOR SPECIFIC CREATIVE NEEDS AT A SPECIFIC PRICE.</p>
-            <img src={process1} alt='process1' size='large'/>
+          <div className='processRow container'>
+            <div className="processItem-ctn container">
+              <p className='headertext processNum'>1.</p>
+              <p className='regtext'>NONPROFITS CREATE A CALL FOR SUBMISSIONS FOR SPECIFIC CREATIVE NEEDS AT A SPECIFIC PRICE.</p>
+              <img className = 'processImg' src={process1} alt='process1'/>
+            </div>
+
+            <div className="processItem-ctn container">
+              <img className = 'processImg' src={process2} alt='process2'/>
+              <p className='headertext processNum'>2.</p>
+              <p className='regtext'>COMMUNITY MEMBERS SUBMIT PITCHES TO OPEN PROJECT REQUESTS IN THEIR FIELD OF EXPERTISE.</p>
+            </div>
           </div>
 
-          <div className="process2 container">
-            <h2>2.</h2><p>COMMUNITY MEMBERS SUBMIT PITCHES TO OPEN PROJECT REQUESTS IN THEIR FIELD OF EXPERTISE.</p>
-            <img src={process2} alt='process2' size='large'/>
-          </div>
+          <br></br>
 
-          <div className="process3 container">
-            <h2>3.</h2><p>THE COMMUNITY ENCOURAGE FELLOW CREATIVES BY HOWLING AT THEIR PROPOSALS TO HELP ORGANIZATIONS PICK THE PERFECT WOLF FOR THEIR PROJECT.</p>
-            <img src={process3} alt='process3' size='large'/>
-          </div>
+          <div className='processRow container'>
+            <div className="processItem-ctn container">
+              <p className='headertext processNum'>3.</p>
+              <p className='regtext'>THE COMMUNITY ENCOURAGE FELLOW CREATIVES BY HOWLING AT THEIR PROPOSALS TO HELP ORGANIZATIONS PICK THE PERFECT WOLF FOR THEIR PROJECT.</p>
+              <img className = 'processImg' src={process3} alt='process3'/>
+            </div>
 
-          <div className="process4 container">
-            <h2>4.</h2><p>AFTER THE SUBMISSION WINDOW CLOSES NONPROFITS CHOOSE THEIR FAVORITE ALLIES TO THEM CREATE MAGIC AND IMPACT.</p>
-            <img src={process4} alt='process4' size='large'/>
+            <div className="processItem-ctn container">
+              <img className = 'processImg' src={process4} alt='process4'/>
+              <p className='headertext processNum'>4.</p>
+              <p className='regtext'>AFTER THE SUBMISSION WINDOW CLOSES NONPROFITS CHOOSE THEIR FAVORITE ALLIES TO THEM CREATE MAGIC AND IMPACT.</p>
+            </div>
           </div>
+        </div>
+
+        <div className='req-invite-ctn process-inv-ctn container'>
+          <p className='regtext'>WORK FOR SOMETING MEANINGFUL | HIRE SOMEONE WHO TRULY CARES</p>
+          <ReqInviteButton/>
         </div>
       </section>
     )
