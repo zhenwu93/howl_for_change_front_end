dfcfb8575c352a489039ff8a4324ab280abf7e9b
zhenwu93 <zhenwu93@gmail.com>
2019-08-07 17:12:05 -0400

    changed soundcloud link

--- src/Components/Event.js
+++ src/Components/Event.js
@@ -426,7 +426,7 @@ class Event extends Component {
               scrolling="no"
               frameBorder="no"
               allow="autoplay"
-              src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/303208989&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+              src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/620223069&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
             />
           </div>
 
