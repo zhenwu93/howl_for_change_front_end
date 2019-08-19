3759d5d22ac13e75c303555d26f22da35c0096a6
Run Zhuo Li <runzhuoli.cs@gmail.com>
2019-08-19 15:03:17 -0400

    add responsive style for landing and whatwedo

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -2,6 +2,7 @@ import React, {Component} from "react"
 import hero from "../assets/hero/hero.jpg"
 import tagline from "../assets/issues/tagline.png"
 import hashtag from '../assets/issues/hashtag.png'
+import './landing.css';
 
 class Landing extends Component {
 
@@ -9,21 +10,14 @@ class Landing extends Component {
     return (
       <section>
         <div className="landing-ctn container">
-
-
             <img src={hero} alt='hero' className='landing-img'/>
-
-
           <div className="tag-line container">
             <img src={tagline} alt="tag" className="tag-line-img"/>
           </div>
-
           <div className='landing-text-div'>
             <p className='landing-text'>JOIN A COMMUNITY OF CREATIVE CHANGEMAKERS AND NONPROFITS WORKING AND HOWLING TOGETHER TO GIVE VOICE TO GLOBAL ISSUES.</p>
             <img align='right' src={hashtag} alt="hashtag" className="hashtag-img"/>
           </div>
-
-
         </div>
       </section>
     )
